01843a908f2df1de04a21e410162c6b0361a8a6f
Robel-Belihu <robelmelaku334@gmail.com>
2023-06-11 12:28:25 +0300

    cleaned up some clutter and changed file extensions

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,7 @@
-import {io} from "socket.io-client"
+import { io } from "socket.io-client";
 import { baseUrl } from "./utils/api";
 // const URL="http://192.168.0.163:3001"
 
 export const socket = io(baseUrl, {
-    autoConnect: false,
-    
-  });
+  autoConnect: false,
+});
